d3396e9df541fab147204f43fcdfb8e388b2b29a
Jordan Lewis <jordan_j_lewis@icloud.com>
2014-11-09 12:13:22 +1100

    Initial Commit

---
+++ JLGlobalAdBanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLGlobalAdBanner
+//
+//  Created by Jordan Lewis on 9/11/2014.
+//  Copyright (c) 2014 Jordan Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
